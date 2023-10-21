70906327a48029627d81bcec124e4e62cf5b6e11
Alexander <52276@etu.he2b.be>
2023-10-22 01:15:09 +0200

    test changing logo

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header(props) {
     return (
-        <header className="bg-">
+        <header className="bg-slate-800 h-24 ">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
                 {props.children}
             </div>
